9b08c1e01c76e5adb1a249dda5a1803ebd358a9e
seungh22 <seunghee8898@naver.com>
2026-01-23 11:20:18 +0900

    refactor: MemberRepository 생성자 주입 전환

--- src/main/java/com/kt/springproject/repository/MemberRepository.java
+++ src/main/java/com/kt/springproject/repository/MemberRepository.java
@@ -2,16 +2,21 @@ package com.kt.springproject.repository;
 
 import com.kt.springproject.domain.Member;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext // 스프링이 EntityManager를 만들어서 인잭션 해줌
-    private EntityManager em;
+//    @PersistenceContext // 스프링이 EntityManager를 만들어서 인잭션 해줌
+    // 스프링 데이터 JPA 사용하면 @Autowired로 바꿀 수 있고 @RequiredArgsConstructor을 사용하여 final로 바꿀 수 있다
+    // 레포지토리에 Entity 매니저를 인젝션 생성자로 인젝션 한 상태
+    @Autowired
+    private final EntityManager em;
 
     public void save(Member member) {
         em.persist(member);
