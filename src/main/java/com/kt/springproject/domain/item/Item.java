1b235b8384bc333c2c240c59e2dba27e18639f17
seungh22 <seunghee8898@naver.com>
2026-01-26 10:36:12 +0900

    feet: 상품 엔티티

--- src/main/java/com/kt/springproject/domain/item/Item.java
+++ src/main/java/com/kt/springproject/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.kt.springproject.domain.item;
 
 import com.kt.springproject.domain.Category;
+import com.kt.springproject.exception.NotEnoughStockException;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,4 +28,22 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
