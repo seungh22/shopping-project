118c326dfa6f69730c14a8076aef84ea1db4dcca
seungh22 <seunghee8898@naver.com>
2026-01-27 11:04:30 +0900

    feet: 주문 리포지토리 개발

---
+++ src/main/java/com/kt/springproject/repository/OrderRepository.java
@@ -0,0 +1,27 @@
+package com.kt.springproject.repository;
+
+import com.kt.springproject.domain.Order;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    //todo: 검색기능
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//    }
+}
