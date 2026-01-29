7b98736f4313732156eda1d24af749725c49a38a
seungh22 <seunghee8898@naver.com>
2026-01-30 04:33:26 +0900

    feat: 회원 목록 조회

--- src/main/java/com/kt/springproject/controller/MemberController.java
+++ src/main/java/com/kt/springproject/controller/MemberController.java
@@ -5,11 +5,14 @@ import com.kt.springproject.domain.Member;
 import com.kt.springproject.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -38,5 +41,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/MemberList";
     }
 }
