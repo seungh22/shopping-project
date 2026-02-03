df1947a9a2001d4902f246afda3ac5e3c13bcdfb
seungh22 <seunghee8898@naver.com>
2026-02-03 12:41:17 +0900

    feat: 상품 등록

---
+++ src/main/java/com/kt/springproject/controller/ItemController.java
@@ -0,0 +1,36 @@
+package com.kt.springproject.controller;
+
+import com.kt.springproject.domain.item.Book;
+import com.kt.springproject.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model) {
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    //todo: setter 제거
+    @PostMapping("/items/new")
+    public String create (BookForm form) {
+        Book book = new Book();
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
