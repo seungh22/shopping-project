d06811f7d4a26d61e232c9e347e9f985ac152874
seungh22 <seunghee8898@naver.com>
2026-02-17 10:52:36 +0900

    feat: V1 엔티티 직접 노출

---
+++ src/main/java/com/kt/springproject/api/OrderSimpleApiController.java
@@ -0,0 +1,33 @@
+package com.kt.springproject.api;
+
+import com.kt.springproject.domain.Order;
+import com.kt.springproject.repository.OrderRepository;
+import com.kt.springproject.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)에서의 성능최적화 방법
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getMember().getAddress(); //Lazy 강제 초기
+        }
+        return all;
+    }
+}
--- src/main/java/com/kt/springproject/domain/OrderItem.java
+++ src/main/java/com/kt/springproject/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package com.kt.springproject.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.kt.springproject.domain.item.Item;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -22,6 +23,7 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
+    @JsonIgnore
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
