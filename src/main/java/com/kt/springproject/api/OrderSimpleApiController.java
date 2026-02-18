782d7cad1694cc408067f21b9fea03401633370e
seungh22 <seunghee8898@naver.com>
2026-02-19 08:26:46 +0900

    feat: V2 엔티티 DTO로 변환

--- src/main/java/com/kt/springproject/api/OrderSimpleApiController.java
+++ src/main/java/com/kt/springproject/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package com.kt.springproject.api;
 
+import com.kt.springproject.domain.Address;
 import com.kt.springproject.domain.Order;
+import com.kt.springproject.domain.OrderStatus;
 import com.kt.springproject.repository.OrderRepository;
 import com.kt.springproject.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)에서의 성능최적화 방법
@@ -29,5 +34,40 @@ public class OrderSimpleApiController {
             order.getMember().getAddress(); //Lazy 강제 초기
         }
         return all;
+    }
+
+    // 엔티티를 DTO로 변환하는 일반적인 방법.
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        //ORDER
+        //N+1 문제
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        //2개
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+    // 쿼리가 총 1+N+N번 실행된다
+    // order 조회 1번 (order 조회 결과 수가 N번 (여기선 2번)
+    // order -> member 지연 로딩 조회 N번 (여기선 2번)
+    // order -> delivery 지연 로딩 조회 N번 (여기선 2번)
+    // 지연 로딩은 영속성 컨텍스트에서(바로 디비 쿼리X) 조회하므로, 이미 조회된 경우 쿼리를 생략한다.
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); //LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); //LAZY 초기화
+        }
     }
 }
